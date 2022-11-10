bf3463e83e4eeccd8660fe37326aaf71e475f754
duong56 <91894323+duong56@users.noreply.github.com>
2022-11-10 12:09:18 +0700

    Change port heroku

--- src/main.ts
+++ src/main.ts
@@ -4,16 +4,15 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const PORT = process.env.PORT || 3000;
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('Dương Lê')
+    .setDescription('The tutorial API description')
     .setVersion('1.0')
-    .addTag('cats')
+    .addTag('Pro')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(PORT);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
