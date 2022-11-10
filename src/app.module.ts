d03498ff6ee1ac0eaacb90a7fd700b25277fbd0d
duong56 <91894323+duong56@users.noreply.github.com>
2022-11-10 18:24:46 +0700

    Update route

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { LoginController } from './login/login.controller';
+import { TestMiddleware } from './test.middleware';
+import { TestController } from './test/test.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [AuthModule],
+  controllers: [AppController, LoginController, TestController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule{}
+//  implements NestModule{
+//   configure(consumer: MiddlewareConsumer) {
+//     consumer.apply(TestMiddleware,).forRoutes('*')
+//   }
+// }
--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   const config = new DocumentBuilder()
-    .setTitle('Dương Lê')
-    .setDescription('The tutorial API description')
-    .setVersion('1.0')
-    .addTag('Pro')
-    .build();
+  .setTitle('Swagger Dương Lê')
+  .setDescription('This is the swagger api')
+  .setVersion('1.0')
+  .build();
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-
-  await app.listen(process.env.PORT || 3000);
+  
+  await app.listen(3000);
 }
 bootstrap();
